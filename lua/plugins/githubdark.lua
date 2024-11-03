7cef8ca875dd90ae361ccfd326b878755baff985
Harshit Kumar <harshit.hk.3012@gmail.com>
2024-11-04 02:55:01 +0530

    update colorscheme

--- lua/plugins/githubdark.lua
+++ lua/plugins/githubdark.lua
@@ -5,9 +5,7 @@ return { -- the colorscheme should be available when starting Neovim
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-        require('github-theme').setup({
-            -- ...
-        })
+       
 
         vim.cmd('colorscheme github_dark_colorblind')
     end
