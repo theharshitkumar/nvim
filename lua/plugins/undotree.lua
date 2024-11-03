e534470cbe39ef0bdf31b377560e83f0d6a01a16
Harshit Kumar <harshit.hk.3012@gmail.com>
2024-11-04 02:55:14 +0530

    working undotree plugin

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,10 +1,16 @@
 return {
     "mbbill/undotree",
-    lazy = false, -- needs to be explicitly set, because of the keys property
-    configs = true,
+    lazy = false,
+    config = function()
+        local utils = require('./utils')
+       
+        if vim.fn.has("win32") == 1 then
+            vim.g.undotree_DiffCommand = utils.get_nvim_path('/bin/diff.exe')
+        end
+    end,
     keys = {{
         "<leader>u",
-        vim.cmd.UndotreeToggle,
+        function() vim.cmd.UndotreeToggle() end,
         desc = "Toggle undotree"
     }}
 }
