7781d877d22385710223d65d3d19370331a59663
Harshit Kumar <harshit.hk.3012@gmail.com>
2024-11-04 00:27:18 +0530

    initial config commit

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,23 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { "python","angular","css", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end
+ }
+
+-- local M = {
+--     "nvim-treesitter/nvim-treesitter",
+--     build = function()
+--         require("nvim-treesitter.install").update({ with_sync = true })()
+--     end,
+-- }
+
+-- return { M }
