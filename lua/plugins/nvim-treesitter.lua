884795b20959b50ad2e4d322a294c1442a00087a
Harshit Kumar <45584381+theharshitkumar@users.noreply.github.com>
2025-07-28 10:30:31 +0530

    update

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -18,11 +18,4 @@ return {
     end
 }
 
--- local M = {
---     "nvim-treesitter/nvim-treesitter",
---     build = function()
---         require("nvim-treesitter.install").update({ with_sync = true })()
---     end,
--- }
 
--- return { M }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
     "nvim-telescope/telescope.nvim",
-    tag = '0.1.8',
+    
     opts = {
         extensions_list = {"fzf"}
     },
